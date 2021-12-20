cef693302d3403c96009b52b8d13cd0d8ba754eb
nando <muhammad.rezki.ananda@gmail.com>
2021-12-20 16:37:26 +0700

    Add view binding & bg shape

--- app/src/main/java/com/motionlaboratory/tugas_1_sg_mp_android_2/activity/MainActivity.kt
+++ app/src/main/java/com/motionlaboratory/tugas_1_sg_mp_android_2/activity/MainActivity.kt
@@ -2,11 +2,18 @@ package com.motionlaboratory.tugas_1_sg_mp_android_2.activity
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import com.motionlaboratory.tugas_1_sg_mp_android_2.R
+import com.motionlaboratory.tugas_1_sg_mp_android_2.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private val binding: ActivityMainBinding by lazy {
+        ActivityMainBinding.inflate(
+            layoutInflater
+        )
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
     }
 }
---
+++ app/src/main/java/com/motionlaboratory/tugas_1_sg_mp_android_2/activity/SplashScreenActivity.kt
@@ -0,0 +1,33 @@
+package com.motionlaboratory.tugas_1_sg_mp_android_2.activity
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import androidx.appcompat.app.AppCompatActivity
+import com.motionlaboratory.tugas_1_sg_mp_android_2.databinding.ActivitySplashScreenBinding
+
+
+class SplashScreenActivity : AppCompatActivity() {
+
+    private val SPLASH_TIME_OUT: Long = 3000
+
+    private val binding: ActivitySplashScreenBinding by lazy {
+        ActivitySplashScreenBinding.inflate(
+            layoutInflater
+        )
+    }
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+//        setupView()
+    }
+
+    private fun setupView() {
+        Handler().postDelayed({
+            startActivity(Intent(this@SplashScreenActivity, MainActivity::class.java))
+            finish()
+        }, SPLASH_TIME_OUT)
+    }
+}
