4b0da74bc36b0d787df0c8ae8269c0b2361ac923
nando <muhammad.rezki.ananda@gmail.com>
2021-12-20 15:50:41 +0700

    INITIAL COMMIT

---
+++ app/src/main/java/com/motionlaboratory/tugas_1_sg_mp_android_2/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.motionlaboratory.tugas_1_sg_mp_android_2
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
---
+++ app/src/test/java/com/motionlaboratory/tugas_1_sg_mp_android_2/ExampleUnitTest.kt
@@ -0,0 +1,16 @@
+package com.motionlaboratory.tugas_1_sg_mp_android_2
+
+import org.junit.Assert.assertEquals
+import org.junit.Test
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+class ExampleUnitTest {
+    @Test
+    fun addition_isCorrect() {
+        assertEquals(4, 2 + 2)
+    }
+}
